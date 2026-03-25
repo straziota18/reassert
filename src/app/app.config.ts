41d70a553c6812f18d607798d583e3d6812c3ef1
Hugo STRAZIOTA <hugo.straziota@gmail.com>
2026-03-25 12:13:50 -0400

    add initialization and active layout logic

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,17 @@
-import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter }           from '@angular/router';
 import { provideHttpClient }       from '@angular/common/http';
 import { routes }                  from './app.routes';
+import { UserSessionService }      from './services/user-session-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(),
+    provideAppInitializer(() => {
+        const userSessionService = inject(UserSessionService);
+        return userSessionService.initialize();
+    })
   ]
 };
--- src/app/services/user-session-service.ts
+++ src/app/services/user-session-service.ts
@@ -1,6 +1,9 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal, WritableSignal } from '@angular/core';
 import { ObjectStoreService } from './object-store-service';
 import { OptimizationService } from './optimization-service';
+import { FactoryLayout } from './model';
+
+const ACTIVE_LAYOUT_KEY = 'reassert:active-layout-id';
 
 @Injectable({
   providedIn: 'root',
@@ -8,4 +11,40 @@ import { OptimizationService } from './optimization-service';
 export class UserSessionService {
   private readonly optimizationService = inject(OptimizationService);
   private readonly objectStoreService = inject(ObjectStoreService);
+
+  readonly activeLayout: WritableSignal<FactoryLayout | null> = signal(null);
+
+  private initEmptyLayout() {
+    const newLayout: FactoryLayout = {
+      id: 'Global layout',
+      factories: [],
+      connections: [],
+    };
+
+    this.objectStoreService.saveLayout(newLayout);
+    localStorage.setItem(ACTIVE_LAYOUT_KEY, newLayout.id);
+    this.activeLayout.set(newLayout);
+  }
+
+  initialize(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      const activeLayoutId = localStorage.getItem(ACTIVE_LAYOUT_KEY);
+
+      if (activeLayoutId) {
+        this.objectStoreService.loadLayout(activeLayoutId).then(layout => {
+          if (layout !== null) {
+            this.activeLayout.set(layout);
+          } else {
+            this.initEmptyLayout();
+          }
+        }).catch(_ => {
+          this.initEmptyLayout();
+        })
+      } else {
+        this.initEmptyLayout();
+      }
+
+      resolve()
+    });
+  }
 }
